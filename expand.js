a5d03b079a5fc5c2a73c6e29cfa19030e0dee97e
bigoldbojo <marcus.mitchell6569@gmail.com>
2021-07-12 17:54:53 +0100

    javascript overhaul

--- expand.js
+++ expand.js
@@ -1,11 +1,41 @@
-function expand(){
+function expand() {
     document.getElementById("expandButton").style.display = "none";
     document.getElementById("collapseButton").style.display = "inline-block";
     document.getElementById("left-panel").style.display = "block";
 }
 
-function collapse(){
+function collapse() {
     document.getElementById("expandButton").style.display = "inline-block";
     document.getElementById("collapseButton").style.display = "none";
     document.getElementById("left-panel").style.display = "none";
+}
+
+function expandButtonMouseDown() {
+    document.getElementById("expandButton").style.backgroundColor = "#111";
+    document.getElementById("expandButton").style.color = "white";
+}
+
+function expandButtonMouseOver() {
+    document.getElementById("expandButton").style.backgroundColor = "#555";
+    document.getElementById("expandButton").style.color = "white";
+}
+
+function expandButtonMouseOut() {
+    document.getElementById("expandButton").style.backgroundColor = "white";
+    document.getElementById("expandButton").style.color = "black";
+}
+
+function collapseButtonMouseDown() {
+    document.getElementById("collapseButton").style.backgroundColor = "#111";
+    document.getElementById("collapseButton").style.color = "white";
+}
+
+function collapseButtonMouseOver() {
+    document.getElementById("collapseButton").style.backgroundColor = "#555";
+    document.getElementById("collapseButton").style.color = "white";
+}
+
+function collapseButtonMouseOut() {
+    document.getElementById("collapseButton").style.backgroundColor = "#333";
+    document.getElementById("collapseButton").style.color = "white";
 }
