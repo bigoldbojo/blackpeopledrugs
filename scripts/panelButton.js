521d1ce2fe5074eb07a5946b931ef93a5222f3ec
bigoldbojo <marcus.mitchell6569@gmail.com>
2021-07-14 02:18:50 +0100

    new filter box for shop

--- scripts/panelButton.js
+++ scripts/panelButton.js
@@ -2,12 +2,14 @@ function expand() {
     document.getElementById("expandButton").style.display = "none";
     document.getElementById("collapseButton").style.display = "inline-block";
     document.getElementById("left-panel").style.display = "block";
+    document.getElementById("filter-box").style.display = "block";
 }
 
 function collapse() {
     document.getElementById("expandButton").style.display = "inline-block";
     document.getElementById("collapseButton").style.display = "none";
     document.getElementById("left-panel").style.display = "none";
+    document.getElementById("filter-box").style.display = "none";
 }
 
 function expandButtonMouseDown() {
