b71b8b525f99768bf28d312d124354352f7697c2
bigoldbojo <marcus.mitchell6569@gmail.com>
2021-07-14 19:08:53 +0100

    gradient buttons

--- scripts/panelButton.js
+++ scripts/panelButton.js
@@ -14,30 +14,36 @@ function collapse() {
 
 function expandButtonMouseDown() {
     document.getElementById("expandButton").style.backgroundColor = "#111";
+    document.getElementById("expandButton").style.backgroundImage = "linear-gradient(to right, #222 , #111);"
     document.getElementById("expandButton").style.color = "white";
 }
 
 function expandButtonMouseOver() {
     document.getElementById("expandButton").style.backgroundColor = "#555";
+    document.getElementById("expandButton").style.backgroundImage = "linear-gradient(to right, #666 , #333);"
     document.getElementById("expandButton").style.color = "white";
 }
 
 function expandButtonMouseOut() {
     document.getElementById("expandButton").style.backgroundColor = "#333";
+    document.getElementById("expandButton").style.backgroundImage = "linear-gradient(to right, #444 , #222);"
     document.getElementById("expandButton").style.color = "white";
 }
 
 function collapseButtonMouseDown() {
     document.getElementById("collapseButton").style.backgroundColor = "#ddd";
+    document.getElementById("collapseButton").style.backgroundImage = "linear-gradient(to right, #fff , #bbb);"
     document.getElementById("collapseButton").style.color = "black";
 }
 
 function collapseButtonMouseOver() {
     document.getElementById("collapseButton").style.backgroundColor = "#aaa";
+    document.getElementById("collapseButton").style.backgroundImage = "linear-gradient(to right, #bbb , #888);"
     document.getElementById("collapseButton").style.color = "black";
 }
 
 function collapseButtonMouseOut() {
     document.getElementById("collapseButton").style.backgroundColor = "white";
+    document.getElementById("collapseButton").style.backgroundImage = "linear-gradient(to right, #ddd , #aaa);"
     document.getElementById("collapseButton").style.color = "black";
 }
